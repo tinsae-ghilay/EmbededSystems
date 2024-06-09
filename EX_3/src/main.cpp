32a1b7e6d53dcfdc8bfb78e61ea7e4c8ab29f4fe
tinsae-ghilay <85639930+tinsae-ghilay@users.noreply.github.com>
2024-06-09 21:26:12 +0200

    cleaned main

--- EX_3/src/main.cpp
+++ EX_3/src/main.cpp
@@ -8,22 +8,8 @@ Ample amp;
 void setup() {
 	Serial.begin(96000);
 	amp.start();
-
-	/*for (int i = 0; i < 5; i++) {
-		digitalWrite(LED_PIN, HIGH);
-		delay(500);
-		digitalWrite(LED_PIN, LOW);
-		delay(500);
-	}*/
 }
 
 void loop() {
-	/*
-	Serial.println(F("Enter sleep mode"));
-	digitalWrite(LED_PIN, HIGH);
-	sleep_delay(7);
-	digitalWrite(LED_PIN, LOW);
-	Serial.println(F("Exit sleep mode"));
-	delay(2000);*/
 	amp.loop();
 }
