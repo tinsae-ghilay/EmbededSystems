8f156071574237fb076d64febbada21b4440ba0e
tinsae-ghilay <85639930+tinsae-ghilay@users.noreply.github.com>
2024-06-16 16:16:35 +0200

    ->

--- Ex_4/src/main.cpp
+++ Ex_4/src/main.cpp
@@ -29,38 +29,55 @@ void setup() {
   sensor.update();
   js.setDeadzone(25);
   js.begin();
-  sensor.switchToContinuousMode();
 }
 
+// converts float to char array
 void floatToChar(char *chr, float f ){
-
+  // because float was printed as '?' when using sprintf(chr, "%.2f",f)
+  // we have to convert decimal and floating point values to uint8_t and append them in sprintf();
+  // first the int part of the float.
   uint8_t tmp = (int) f;
+  // and the decimal part as integer -> float - the int part multiplied by 100 -> (because 2 nachkommazahlen)
   uint8_t tmp_fl = (f - tmp) *100;
-  sprintf (chr, "%d.%02d ", tmp, tmp_fl); //
+  // and we transfer it to char array
+  sprintf (chr, "%d.%02d ", tmp, tmp_fl); 
 }
+
+// breaking time stamp 
+uint16_t start = millis();
+// counter to help us count 3 seconds
+int step = 0;
 
 // loop
-uint16_t start = millis();
-int step = 0;
 void loop() {
+  // get the saved temprature value
   float temp = sensor.getTemperature(false);
+  // convert it to char array, because ledMatrix doesnt accept floats
   char text[6];
   floatToChar(text,temp);
+  // draw text
   textBuffer.drawText(text);
   textBuffer.update();
+  // current timestamp
   uint16_t now = millis();
+  // update joystick to get its status
   js.update();
   // if joystick button is pressed text will stop sliding to the left
   if(now-start > 100 && !js.getButton()){
     textBuffer.slideLeft(1);
+    // every 100 millisecond is counted as one
     step++;
+    // breaking timestamp
     start = now;
   }
   // update temprature every 3 seconds
   if(step > 30){
+    // update sensor to save new temprature value
     sensor.update();
+    // printing to sensor
     Serial.print("Current temprature: ");
     Serial.println(temp);
+    // 3 seconds have passed, so we reset our counter
     step = 0;
   }
 }
